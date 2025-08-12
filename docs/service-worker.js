7b364090e798a581c307e410ebc33fcc3aed7e65
justeyoung <justeyoung@yahoo.com>
2025-08-12 20:46:43 +0100

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,28 +1,51 @@
-const cacheName = "timer-cache-v1";
-const assets = [
-  "/counter.html",
-  "/counter.js",
-  "/counter.css",
-  "/click.mp3",
-  "/alert.wav",
-  "/icon-192.png",
-  "/icon-512.png",
-  "/manifest.json"
+const CACHE_NAME = "exercise-tools-v1";
+const FILES_TO_CACHE = [
+  "index.html",
+  "counter.html",
+  "interval.html",
+  "style.css",
+  "counter.css",
+  "interval.css",
+  "script.js",
+  "counter.js",
+  "interval.js",
+  "docs/icon-192.png",
+  "docs/icon-512.png",
+  "docs/click.mp3",
+  "docs/alert.wav",
+  "manifest.json"
 ];
 
-// Cache on install
-self.addEventListener("install", (event) => {
+// Install service worker and cache files
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(assets);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
+  self.skipWaiting();
 });
 
-// Serve from cache
-self.addEventListener("fetch", (event) => {
+// Activate and clean up old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch files from cache first, then network
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
